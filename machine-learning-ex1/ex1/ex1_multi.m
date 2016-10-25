1f9598444e4e8b3def4ac26a9c6fff39692d33d3
Rodrigo de Bona Sartor <rodrigo@kugel.com.br>
2016-10-25 13:08:56 -0200

    Previsão do preço utilizando gradient descent e normal equation

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,7 +82,7 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.01;
+alpha = 0.10;
 num_iters = 400;
 
 % Init Theta and Run Gradient Descent 
@@ -104,7 +104,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+E = ( [ 1650, 3 ] - mu ) ./ sigma;
+price = [1, E] * theta;
 
 
 % ============================================================
@@ -149,7 +150,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [ 1, 1650, 3 ] * theta;
 
 
 % ============================================================
